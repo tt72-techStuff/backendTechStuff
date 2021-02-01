c62a3f261f8c6f5a171c6a067f423d1cc7e5c413
reesharper <reestimharper@gmail.com>
2021-02-01 17:37:28 -0500

    gabe to the rescue?

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,29 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/users.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
+
+
 // production: {
 //   client: "pg",
 //   connection: pgConnection,

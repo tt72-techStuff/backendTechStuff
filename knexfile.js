9db1b712792d596220504d788e2ca388e330d8d2
reesharper <reestimharper@gmail.com>
2021-02-02 17:38:38 -0500

    ignor

--- knexfile.js
+++ knexfile.js
@@ -1,62 +1,23 @@
 require("dotenv").config();
-
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
-
+const pg = require('pg')
+const localConnection = process.env.REES
+let connection
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+  connection = process.env.DATABASE_URL
+} else {
+  connection = localConnection
+}
+const sharedConfig = {
+  client: 'pg',
+  connection,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/users.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
-  
+  development: { ...sharedConfig, localConnection},
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/users.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
+    ...sharedConfig,
+    pool: { min: 2, max: 10 },
   },
-};
-
-
-// production: {
-//   client: "pg",
-//   connection: pgConnection,
-//   pool: {
-//     min: 2,
-//     max: 10,
-//   },
-//   migrations: {
-//     directory: "./database/migrations",
-//   },
-//   seeds: {
-//     directory: "./database/seeds",
-//   },
-// },
+}

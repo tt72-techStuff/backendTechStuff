a966c7356e667695c48decc6b649a8adf0391e8d
reesharper <reestimharper@gmail.com>
2021-02-02 18:19:35 -0500

    back to my knex lol

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,22 @@
-require('dotenv').config()
 const pg = require('pg')
-
+const localConnection = 'postgres://postgres:Harperr23@localhost:5432/build_week'
+let connection
 if (process.env.DATABASE_URL) {
   pg.defaults.ssl = { rejectUnauthorized: false }
+  connection = process.env.DATABASE_URL
+} else {
+  connection = localConnection
 }
-
 const sharedConfig = {
   client: 'pg',
-  migrations: { directory: './api/data/migrations' },
-  seeds: { directory: './api/data/seeds' },
+  connection,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
 }
-
 module.exports = {
-  development: {
-    ...sharedConfig,
-    connection: process.env.DEV_DATABASE_URL
-  },
+  development: { ...sharedConfig, },
   production: {
     ...sharedConfig,
-    connection: process.env.DATABASE_URL,
     pool: { min: 2, max: 10 },
   },
 }

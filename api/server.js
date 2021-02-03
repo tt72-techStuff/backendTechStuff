d8f3729a9fd44c0d74176286685fd0ec7c020537
reesharper <reestimharper@gmail.com>
2021-02-03 15:36:10 -0500

    cors and helmet

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const helmet = require("helmet");
-// const cors = require("cors");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const authRouter = require("./auth/auth-router.js");
 const usersRouter = require("./users/users-router.js");
@@ -8,9 +8,9 @@ const postsRouter = require("./posts/posts-router.js");
 
 const server = express();
 
-// server.use(helmet());
+server.use(helmet());
 server.use(express.json());
-// server.use(cors());
+server.use(cors());
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);

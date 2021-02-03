2d19896c32d5849aa7fa502a89ed16b9465aaf5b
reesharper <reestimharper@gmail.com>
2021-02-03 13:09:59 -0500

    asdf

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 const pg = require('pg')
-const localConnection = 'postgresql://postgres:Harperr23@localhost/rentmytech'
+const localConnection = 'postgresql://postgres:Harperr23@localhost:5432/rentmytech'
 let connection
 if (process.env.DATABASE_URL) {
   pg.defaults.ssl = { rejectUnauthorized: false }

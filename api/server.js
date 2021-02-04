34b662fd925259813cb5817efb69ac0ce2423819
reesharper <reestimharper@gmail.com>
2021-02-04 05:45:26 -0500

    documentation

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+// const swaggerJsDoc = require('swagger-jsdoc')
+// const swaggerUI = require('swagger-ui-express')
 
 const authRouter = require("./auth/auth-router.js");
 const usersRouter = require("./users/users-router.js");
